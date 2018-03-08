24923a8010a456140b79d302ca8eecb9fb8ef87e
jedan-covik <28544642+jedan-covik@users.noreply.github.com>
2018-03-08 19:50:37 +0100

    Added database context

---
+++ BettingSite/Models/BettingSiteContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BettingSite.Models
+{
+    public class BettingSiteContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public BettingSiteContext() : base("name=BettingSiteContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<BettingSite.Models.Sport> Sports { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<BettingSiteContext>(new DropCreateDatabaseIfModelChanges<BettingSiteContext>());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
