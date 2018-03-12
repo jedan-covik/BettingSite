7d2fb5072e6f8980e1b0839c72b17f571d9cbb26
jedan-covik <28544642+jedan-covik@users.noreply.github.com>
2018-03-12 20:55:59 +0100

    Added aditional seed data

--- BettingSite/Database/MockDataDBInitializer.cs
+++ BettingSite/Database/MockDataDBInitializer.cs
@@ -22,14 +22,21 @@ namespace BettingSite.Database
             context.Teams.Add(new Team { teamId = 2, name = "Dinamo Zagreb", sportId = 1 });
             context.Teams.Add(new Team { teamId = 3, name = "Zadar", sportId = 1 });
 
+            context.Teams.Add(new Team { teamId = 4, name = "Zadar", sportId = 2 });
+            context.Teams.Add(new Team { teamId = 5, name = "Split", sportId = 2 });
+
             context.Matches.Add(new Match { matchId = 1, homeTeamId = 1, guestTeamId = 2, matchQuota = (decimal)1.4, matchDateTime = new DateTime(2018, 3, 16, 18, 0, 0) });
             context.Matches.Add(new Match { matchId = 2, homeTeamId = 3, guestTeamId = 2, matchQuota = (decimal)1.8, matchDateTime = new DateTime(2018, 3, 22, 18, 0, 0) });
+            context.Matches.Add(new Match { matchId = 3, homeTeamId = 3, guestTeamId = 1, matchQuota = (decimal)1.6, matchDateTime = new DateTime(2018, 3, 26, 18, 0, 0) });
+
+            context.Matches.Add(new Match { matchId = 4, homeTeamId = 5, guestTeamId = 4, matchQuota = (decimal)1.6, matchDateTime = new DateTime(2018, 3, 24, 18, 0, 0) });
+
 
             context.Wallets.Add(new Wallet { walletId = 1, amount = 100 });
 
-            context.Tickets.Add(new Ticket { ticketId = 1, totalWager = 50, walletId = 1 });
+            context.Tickets.Add(new Ticket { ticketId = 1, totalWager = 50, walletId = 1, totalQuota = (decimal)1.4 });
 
-            context.TicketWagers.Add(new TicketWagers { ticketWagerId = 1, ticketId = 1, matchId = 1, wagerTypeId = 1});
+            context.TicketWagers.Add(new TicketWagers { ticketWagerId = 1, ticketId = 1, matchId = 1, wagerTypeId = 1, matchQuota = (decimal)1.4 });
 
             context.SaveChanges();
         }
